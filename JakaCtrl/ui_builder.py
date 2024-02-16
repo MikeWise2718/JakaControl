d11b8983d92af6bc17d8d26227008838f6fbe60b
MikeWise2718 <mwise@microsoft.com>
2024-02-16 08:06:24 +0100

    variable robot and colors

--- JakaCtrl/ui_builder.py
+++ JakaCtrl/ui_builder.py
@@ -9,6 +9,7 @@
 #
 
 import numpy as np
+import math
 import omni.timeline
 import omni.ui as ui
 from omni.isaac.core.articulations import Articulation
@@ -18,6 +19,8 @@ from omni.isaac.core.utils.nucleus import get_assets_root_path
 from omni.isaac.core.utils.prims import is_prim_path_valid
 from omni.isaac.core.utils.stage import add_reference_to_stage, create_new_stage, get_current_stage
 from omni.isaac.core.world import World
+from omni.ui import Button
+from omni.ui import color as uiclr
 from omni.isaac.ui.element_wrappers import CollapsableFrame, StateButton
 from omni.isaac.ui.element_wrappers.core_connectors import LoadButton, ResetButton
 from omni.isaac.ui.ui_utils import get_style
@@ -26,8 +29,66 @@ from pxr import Sdf, UsdLux, UsdPhysics, Usd
 
 from .scenario import ExampleScenario
 
+import carb.settings
+
+
+_settings = None
+
+
+def _init_settings():
+    global _settings
+    if _settings is None:
+        _settings = carb.settings.get_settings()
+    return _settings
+
+
+def get_setting(name, default, db=False):
+    settings = _init_settings()
+    key = f"/persistent/omni/sphereflake/{name}"
+    val = settings.get(key)
+    if db:
+        oval = val
+        if oval is None:
+            oval = "None"
+    if val is None:
+        val = default
+    if db:
+        print(f"get_setting {name} {oval} {val}")
+    return val
+
+
+def save_setting(name, value):
+    settings = _init_settings()
+    key = f"/persistent/omni/sphereflake/{name}"
+    settings.set(key, value)
+
+
+def truncf(number, digits) -> float:
+    # Improve accuracy with floating point operations, to avoid truncate(16.4, 2) = 16.39 or truncate(-1.13, 2) = -1.12
+    nbDecimals = len(str(number).split('.')[1])
+    if nbDecimals <= digits:
+        return number
+    stepper = 10.0 ** digits
+    return math.trunc(stepper * number) / stepper
+
+
 
 class UIBuilder:
+    btgreen = uiclr("#00ff00")
+    btblue = uiclr("#0000ff")
+    btred = uiclr("#ff0000")
+    btyellow = uiclr("#ffff00")
+    btpurple = uiclr("#ff00ff")
+    btcyan = uiclr("#00ffff")
+    dkgreen = uiclr("#004000")
+    dkblue = uiclr("#000040")
+    dkred = uiclr("#400000")
+    dkyellow = uiclr("#404000")
+    dkpurple = uiclr("#400040")
+    dkcyan = uiclr("#004040")
+    _robot_names = ["ur10e", "jaka", "franka"]
+    _robot_name = "jaka"
+
     def __init__(self):
         # Frames are sub-windows that can contain multiple UI elements
         self.frames = []
@@ -39,6 +100,19 @@ class UIBuilder:
 
         # Run initialization for the provided example
         self._on_init()
+
+
+    def SaveSettings(self):
+        print("SaveSettings")
+        try:
+            save_setting("p_robot_name", self._robot_name)
+
+        except Exception as e:
+            carb.log_error(f"Exception in SaveSettings: {e}")
+
+    def LoadSettings(self):
+        print("LoadSettings")
+        self._robot_name = get_setting("p_robot_name", self._robot_name)
 
     ###################################################################################
     #           The Functions Below Are Called Automatically By extension.py
@@ -92,30 +166,42 @@ class UIBuilder:
         for ui_elem in self.wrapped_ui_elements:
             ui_elem.cleanup()
 
+        self.SaveSettings()
+
     def build_ui(self):
         """
         Build a custom UI tool to run your extension.
         This function will be called any time the UI window is closed and reopened.
         """
+        world_config_frame = CollapsableFrame("World Config", collapsed=False)
+
+        with world_config_frame:
+            with ui.VStack(style=get_style(), spacing=5, height=0):
+                with ui.HStack(style=get_style(), spacing=5, height=0):
+                    ui.Label("Robot: ",
+                            style={'color': self.btyellow},
+                            width=50)
+                    self._robot_btn = Button(
+                        self._robot_name, clicked_fn=self._change_robot_name,
+                        style={'background_color': self.dkgreen},
+                    )
+                # self.wrapped_ui_elements.append(self._robot_btn)
+
+
+
         world_controls_frame = CollapsableFrame("World Controls", collapsed=False)
 
         with world_controls_frame:
             with ui.VStack(style=get_style(), spacing=5, height=0):
                 self._load_btn = LoadButton(
-                    "Load ur10e", "LOAD ur10e", setup_scene_fn=self._setup_scene, setup_post_load_fn=self._setup_scenario
-                )
-                self._load_btn.set_world_settings(physics_dt=1 / 60.0, rendering_dt=1 / 60.0)
-                self.wrapped_ui_elements.append(self._load_btn)
-
-                self._load_btn = LoadButton(
-                    "Load Jaka", "LOAD jaka", setup_scene_fn=self._setup_scene_jaka, setup_post_load_fn=self._setup_scenario
+                    "Load/Create Scene", "Create", setup_scene_fn=self._setup_scene_gen, setup_post_load_fn=self._setup_scenario
                 )
                 self._load_btn.set_world_settings(physics_dt=1 / 60.0, rendering_dt=1 / 60.0)
                 self.wrapped_ui_elements.append(self._load_btn)
 
 
                 self._reset_btn = ResetButton(
-                    "Reset Button", "RESET", pre_reset_fn=None, post_reset_fn=self._on_post_reset_btn
+                    "Reset Button", "Reset", pre_reset_fn=None, post_reset_fn=self._on_post_reset_btn
                 )
                 self._reset_btn.enabled = False
                 self.wrapped_ui_elements.append(self._reset_btn)
@@ -126,8 +212,8 @@ class UIBuilder:
             with ui.VStack(style=get_style(), spacing=5, height=0):
                 self._scenario_state_btn = StateButton(
                     "Run Scenario",
-                    "RUN",
-                    "STOP",
+                    "Run",
+                    "Stop",
                     on_a_click_fn=self._on_run_scenario_a_text,
                     on_b_click_fn=self._on_run_scenario_b_text,
                     physics_callback_fn=self._update_scenario,
@@ -143,6 +229,7 @@ class UIBuilder:
         self._articulation = None
         self._cuboid = None
         self._scenario = ExampleScenario()
+        self.LoadSettings()
 
     def _add_light_to_stage(self):
         """
@@ -153,33 +240,95 @@ class UIBuilder:
         sphereLight.CreateIntensityAttr(100000)
         XFormPrim(str(sphereLight.GetPath())).set_world_pose([6.5, 0, 12])
 
-    def _setup_scene(self):
-        """
-        This function is attached to the Load Button as the setup_scene_fn callback.
-        On pressing the Load Button, a new instance of World() is created and then this function is called.
-        The user should now load their assets onto the stage and add them to the World Scene.
+    # def _setup_scene(self):
+    #     """
+    #     This function is attached to the Load Button as the setup_scene_fn callback.
+    #     On pressing the Load Button, a new instance of World() is created and then this function is called.
+    #     The user should now load their assets onto the stage and add them to the World Scene.
 
-        In this example, a new stage is loaded explicitly, and all assets are reloaded.
-        If the user is relying on hot-reloading and does not want to reload assets every time,
-        they may perform a check here to see if their desired assets are already on the stage,
-        and avoid loading anything if they are.  In this case, the user would still need to add
-        their assets to the World (which has low overhead).  See commented code section in this function.
-        """
-        # Load the UR10e
-        robot_prim_path = "/ur10e"
-        path_to_robot_usd = get_assets_root_path() + "/Isaac/Robots/UniversalRobots/ur10e/ur10e.usd"
-        print("Loading ur10e")
-        print(f"Assets root path: {get_assets_root_path()}")
-        # robot_prim_path = "/jaka"
-        # path_to_robot_usd = "D:/nv/articulation_inspector/minicobo_v14"
-        # print("Loading Jaka ")
+    #     In this example, a new stage is loaded explicitly, and all assets are reloaded.
+    #     If the user is relying on hot-reloading and does not want to reload assets every time,
+    #     they may perform a check here to see if their desired assets are already on the stage,
+    #     and avoid loading anything if they are.  In this case, the user would still need to add
+    #     their assets to the World (which has low overhead).  See commented code section in this function.
+    #     """
+    #     # Load the UR10e
+    #     robot_prim_path = "/ur10e"
+    #     path_to_robot_usd = get_assets_root_path() + "/Isaac/Robots/UniversalRobots/ur10e/ur10e.usd"
+    #     print("Loading ur10e")
 
-        # Do not reload assets when hot reloading.  This should only be done while extension is under development.
-        # if not is_prim_path_valid(robot_prim_path):
-        #     create_new_stage()
-        #     add_reference_to_stage(path_to_robot_usd, robot_prim_path)
-        # else:
-        #     print("Robot already on Stage")
+    #     create_new_stage()
+    #     self._add_light_to_stage()
+    #     add_reference_to_stage(path_to_robot_usd, robot_prim_path)
+
+    #     # Create a cuboid
+    #     self._cuboid = FixedCuboid(
+    #         "/Scenario/cuboid", position=np.array([0.3, 0.3, 0.5]), size=0.05, color=np.array([255, 0, 0])
+    #     )
+
+    #     self._articulation = Articulation(robot_prim_path)
+
+    #     # Add user-loaded objects to the World
+    #     world = World.instance()
+    #     world.scene.add(self._articulation)
+    #     world.scene.add(self._cuboid)
+
+    # def _setup_scene_jaka(self):
+    #     """
+    #     This function is attached to the Load Button as the setup_scene_fn callback.
+    #     On pressing the Load Button, a new instance of World() is created and then this function is called.
+    #     The user should now load their assets onto the stage and add them to the World Scene.
+
+    #     In this example, a new stage is loaded explicitly, and all assets are reloaded.
+    #     If the user is relying on hot-reloading and does not want to reload assets every time,
+    #     they may perform a check here to see if their desired assets are already on the stage,
+    #     and avoid loading anything if they are.  In this case, the user would still need to add
+    #     their assets to the World (which has low overhead).  See commented code section in this function.
+    #     """
+
+    #     robot_prim_path = "/World/minicobo_v1_4/world"
+    #     path_to_robot_usd = "d:/nv/ov/exts/JakaControl/usd/jaka1_blue.usda"
+    #     print("Loading Jaka ")
+
+
+    #     create_new_stage()
+    #     self._add_light_to_stage()
+    #     add_reference_to_stage(path_to_robot_usd, robot_prim_path)
+
+    #     # Create a cuboid
+    #     self._cuboid = FixedCuboid(
+    #         "/Scenario/cuboid", position=np.array([0.3, 0.3, 0.5]), size=0.05, color=np.array([255, 0, 0])
+    #     )
+    #     prim = get_current_stage().GetPrimAtPath(robot_prim_path)
+    #     UsdPhysics.ArticulationRootAPI.Apply(prim)
+
+    #     self._articulation = Articulation(robot_prim_path)
+    #     print("Jaka Articulation: ", self._articulation)
+
+    #     # Add user-loaded objects to the World
+    #     world = World.instance()
+    #     world.scene.add(self._articulation)
+    #     print("Jaka added articulation to world")
+    #     world.scene.add(self._cuboid)
+
+    def _setup_scene_gen(self):
+
+        match self._robot_name:
+            case "ur10e":
+                robot_prim_path = "/ur10e"
+                path_to_robot_usd = get_assets_root_path() + "/Isaac/Robots/UniversalRobots/ur10e/ur10e.usd"
+            case "jaka":
+                robot_prim_path = "/World/minicobo_v1_4/world"
+                path_to_robot_usd = "d:/nv/ov/exts/JakaControl/usd/jaka1_blue.usda"
+            case "franka":
+                robot_prim_path = "/ur10e"
+                path_to_robot_usd = get_assets_root_path() + "/Isaac/Robots/UniversalRobots/ur10e/ur10e.usd"
+            case _:
+                self.error(f"Unknown robot name {self._robot_name}")
+
+
+        print(f"Loading {self._robot_name}")
+
 
         create_new_stage()
         self._add_light_to_stage()
@@ -189,6 +338,9 @@ class UIBuilder:
         self._cuboid = FixedCuboid(
             "/Scenario/cuboid", position=np.array([0.3, 0.3, 0.5]), size=0.05, color=np.array([255, 0, 0])
         )
+        if self._robot_name == "jaka":
+            prim = get_current_stage().GetPrimAtPath(robot_prim_path)
+            UsdPhysics.ArticulationRootAPI.Apply(prim)
 
         self._articulation = Articulation(robot_prim_path)
 
@@ -197,55 +349,12 @@ class UIBuilder:
         world.scene.add(self._articulation)
         world.scene.add(self._cuboid)
 
-    def _setup_scene_jaka(self):
-        """
-        This function is attached to the Load Button as the setup_scene_fn callback.
-        On pressing the Load Button, a new instance of World() is created and then this function is called.
-        The user should now load their assets onto the stage and add them to the World Scene.
 
-        In this example, a new stage is loaded explicitly, and all assets are reloaded.
-        If the user is relying on hot-reloading and does not want to reload assets every time,
-        they may perform a check here to see if their desired assets are already on the stage,
-        and avoid loading anything if they are.  In this case, the user would still need to add
-        their assets to the World (which has low overhead).  See commented code section in this function.
-        """
-        # Load the UR10e
-        #robot_prim_path = "/ur10e"
-        #path_to_robot_usd = get_assets_root_path() + "/Isaac/Robots/UniversalRobots/ur10e/ur10e.usd"
-        #print("Loading ur10e")
-        #print(f"Assets root path: {get_assets_root_path()}")
-        robot_prim_path = "/World/minicobo_v1_4/world"
-        # path_to_robot_usd = "d:/nv/articulation_inspector/minicobo_v14/minicobo_blue_1.usda"
-        path_to_robot_usd = "d:/nv/ov/exts/JakaControl/usd/jaka1_blue.usda"
-        print("Loading Jaka ")
-
-        # Do not reload assets when hot reloading.  This should only be done while extension is under development.
-        # if not is_prim_path_valid(robot_prim_path):
-        #     create_new_stage()
-        #     add_reference_to_stage(path_to_robot_usd, robot_prim_path)
-        # else:
-        #     print("Robot already on Stage")
-
-        create_new_stage()
-        self._add_light_to_stage()
-        add_reference_to_stage(path_to_robot_usd, robot_prim_path)
-
-        # Create a cuboid
-        self._cuboid = FixedCuboid(
-            "/Scenario/cuboid", position=np.array([0.3, 0.3, 0.5]), size=0.05, color=np.array([255, 0, 0])
-        )
-        prim = get_current_stage().GetPrimAtPath(robot_prim_path)
-        UsdPhysics.ArticulationRootAPI.Apply(prim)
-
-        self._articulation = Articulation(robot_prim_path)
-        print("Jaka Articulation: ", self._articulation)
-
-        # Add user-loaded objects to the World
-        world = World.instance()
-        world.scene.add(self._articulation)
-        print("Jaka added articulation to world")
-        world.scene.add(self._cuboid)
-
+    def _change_robot_name(self):
+        idx = self._robot_names.index(self._robot_name)
+        idx = (idx + 1) % len(self._robot_names)
+        self._robot_name = self._robot_names[idx]
+        self._robot_btn.text = self._robot_name
 
     def _setup_scenario(self):
         """
